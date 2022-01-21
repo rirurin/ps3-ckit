79d91370e4c0ead06dd451efce9351d0bd520f7a
Secre-C <89033534+Secre-C@users.noreply.github.com>
2022-01-20 23:57:48 -0800

    added code for foggy glasses

--- prx/modules/p5/SecreC/SecreC.c
+++ prx/modules/p5/SecreC/SecreC.c
@@ -23,55 +23,28 @@
 
 
 // You need to declare hooks with SHK_HOOK before you can use them.
-SHK_HOOK( void, FUN_004eae40, u16* param_1, int param_2, int param_3 );
-//SHK_HOOK( void, FUN_005528f8, undefined4* param_1 );
+SHK_HOOK( undefined8, FUN_004eaca4, int param_1 );
 
 // The start function of the PRX. This gets executed when the loader loads the PRX at boot.
 // This means game data is not initialized yet! If you want to modify anything that is initialized after boot,
 // hook a function that is called after initialisation.
-static void FUN_004eae40Hook( u16* param_1, int param_2, int param_3 )
-{	
-	if ( GetBitflagState( 0x2130 ) == 0 )
-	{
-		//char bufferThing[64];
-		u8 bVar1;
-		SHK_CALL_HOOK( FUN_004eae40, param_1, param_2, param_3 );
-		bVar1 = *(u8 *)((int)param_1 + 9);
-		//bVar1 = (bVar1) / ((int)param_1 + 9);
-		printf( "titties %d\n", bVar1 );
-	}
-	else
-	{
-		SHK_CALL_HOOK( FUN_004eae40, param_1, param_2, param_3 );
-	}
-	return;
-}
 
-/*static void FUN_005528f8Hook(undefined4* param_1)
-
+undefined8 SelPortraitFromModelID(int param_1)
 {
-	undefined4* puVar4;
-	SHK_CALL_HOOK( FUN_005528f8, param_1 );
-	if ( GetTotalDays() >= 214 )
-    {
-		puVar4 = sprintf("field/panel/wipe/wipeParallelTwistPack.pac", 0);
-	}
-	else
+	undefined8 result = SHK_CALL_HOOK( FUN_004eaca4, param_1 );
+	if ( GetBitflagState( 2815 ) == 1 && GetTotalDays() == 231 )
 	{
-		puVar4 = sprintf("field/panel/wipe/wipeParallelPack.pac", 0);
+		*(undefined *)(param_1 + 9) = 5;
 	}
-  *param_1 = puVar4;
-  *(undefined *)(param_1 + 0x45) = 1;
-  return;
-} */
+	return result;
+}
 
 void SecreCInit( void )
 {
   // Hooks must be 'bound' to a handler like this in the start function.
   // If you don't do this, the game will crash.
   
-  SHK_BIND_HOOK( FUN_004eae40, FUN_004eae40Hook );
-  //SHK_BIND_HOOK( FUN_005528f8, FUN_005528f8Hook );
+  SHK_BIND_HOOK( FUN_004eaca4, SelPortraitFromModelID);
 }
 
 void SecreCShutdown( void )

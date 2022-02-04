76f17169445aa84ea4952b8e0ca6792c8f3c3a41
Secre-C <89033534+Secre-C@users.noreply.github.com>
2022-02-03 23:55:40 -0800

    added stuff to make Kasumi/Sumire work as one confidant

--- prx/modules/p5/SecreC/SecreC.c
+++ prx/modules/p5/SecreC/SecreC.c
@@ -24,6 +24,9 @@
 
 // You need to declare hooks with SHK_HOOK before you can use them.
 SHK_HOOK( undefined8, FUN_004eaca4, int param_1 );
+SHK_HOOK( void, FUN_005528f8, ParallelWipe *a1 );
+SHK_HOOK( u64, FUN_00244250, CmmStruct *a1, double a2 );
+SHK_HOOK( undefined8, FUN_0023c070, undefined8 a1, u32 a2, int a3 );
 
 // The start function of the PRX. This gets executed when the loader loads the PRX at boot.
 // This means game data is not initialized yet! If you want to modify anything that is initialized after boot,
@@ -39,12 +42,47 @@ undefined8 SelPortraitFromModelID(int param_1)
 	return result;
 }
 
+void WipeParallelHook( ParallelWipe *a1 ) // needs - [ be32, 0x552960, 0x60000000 ] #removes og metaverse wipe call
+{
+	SHK_CALL_HOOK( FUN_005528f8, a1 );
+	/*if (GetTotalDays() >= 275 )
+	{
+	a1->Wipe_Pac = open_file("field/panel/wipe/wipeParallelTwistPack.pac",1);
+	}
+	else
+	{
+	a1->Wipe_Pac = open_file("field/panel/wipe/wipeParallelPack.pac",1);
+	} */
+	return; 
+}
+
+u64 RankupCardHook( CmmStruct *a1, double a2 ) //changes Kasumi card to Sumire's if the date is past 1/1 or later
+ {
+	if (GetTotalDays() >= 275 && a1->Field52->cardId == 21)
+	{
+		a1->Field52->cardId = 23;
+	}
+	return SHK_CALL_HOOK( FUN_00244250, a1, a2 );
+ }
+
+undefined8 ConfRankupHook( undefined8 a1, u32 a2, int a3 ) //makes kasumi rankup trigger sumire rankup animation if the date is 1/1 or later
+{
+	if ( a2 == 33 && GetTotalDays() >= 275 )
+	{
+		a2 = 36;
+	}
+	SHK_CALL_HOOK( FUN_0023c070, a1, a2, a3 );
+}
+
 void SecreCInit( void )
 {
   // Hooks must be 'bound' to a handler like this in the start function.
   // If you don't do this, the game will crash.
   
   SHK_BIND_HOOK( FUN_004eaca4, SelPortraitFromModelID);
+  SHK_BIND_HOOK( FUN_005528f8, WipeParallelHook );
+  SHK_BIND_HOOK( FUN_00244250, RankupCardHook );
+  SHK_BIND_HOOK( FUN_0023c070, ConfRankupHook );
 }
 
 void SecreCShutdown( void )
--- prx/modules/p5/SecreC/SecreC.h
+++ prx/modules/p5/SecreC/SecreC.h
@@ -12,6 +12,77 @@ void SecreCShutdown( void );
 
 #include "modules/p5/p5.h"
 
+typedef struct  
+{
+    undefined4 * Wipe_Pac;
+}ParallelWipe;
+
+typedef struct
+{
+    u32 field00;
+    u32 field04;
+    u32 field08;
+    u32 field0C;
+    undefined4 cardId;
+}cmmFormatStruct;
+
+typedef struct
+{
+    undefined field0_0x0;
+    undefined field1_0x1;
+    undefined field2_0x2;
+    undefined field3_0x3;
+    undefined field4_0x4;
+    undefined field5_0x5;
+    undefined field6_0x6;
+    undefined field7_0x7;
+    undefined field8_0x8;
+    undefined field9_0x9;
+    undefined field10_0xa;
+    undefined field11_0xb;
+    undefined field12_0xc;
+    undefined field13_0xd;
+    undefined field14_0xe;
+    undefined field15_0xf;
+    undefined field16_0x10;
+    undefined field17_0x11;
+    undefined field18_0x12;
+    undefined field19_0x13;
+    undefined field20_0x14;
+    undefined field21_0x15;
+    undefined field22_0x16;
+    undefined field23_0x17;
+    undefined field24_0x18;
+    undefined field25_0x19;
+    undefined field26_0x1a;
+    undefined field27_0x1b;
+    undefined field28_0x1c;
+    undefined field29_0x1d;
+    undefined field30_0x1e;
+    undefined field31_0x1f;
+    undefined field32_0x20;
+    undefined field33_0x21;
+    undefined field34_0x22;
+    undefined field35_0x23;
+    undefined field36_0x24;
+    undefined field37_0x25;
+    undefined field38_0x26;
+    undefined field39_0x27;
+    undefined field40_0x28;
+    undefined field41_0x29;
+    undefined field42_0x2a;
+    undefined field43_0x2b;
+    undefined field44_0x2c;
+    undefined field45_0x2d;
+    undefined field46_0x2e;
+    undefined field47_0x2f;
+    undefined field48_0x30;
+    undefined field49_0x31;
+    undefined field50_0x32;
+    undefined field51_0x33;
+    cmmFormatStruct * Field52;
+}CmmStruct;
+
 
 #endif
 #endif

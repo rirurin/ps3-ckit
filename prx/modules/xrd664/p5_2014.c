c07feadf6f6f7a0f58004cda76882c1b220b7538
Rirurin <rirurin@rirurin.com>
2024-03-29 01:52:58 +0800

    fix camp persona menu

--- prx/modules/xrd664/p5_2014.c
+++ prx/modules/xrd664/p5_2014.c
@@ -25,6 +25,9 @@
 SHK_HOOK( u32, initReadUpdate, gfdTask* id, float deltaTime );
 SHK_HOOK( u32, fldMainUpdate, gfdTask* id, float deltaTime );
 SHK_HOOK( u32, gfdAnimationStreamRead, gfdStream* pStream);
+SHK_HOOK( void, shdPersonaModelUpdate, _shd_persona_disp_control_type* work);
+
+shdPMDD shdPersonaStats[464] = {0, 0, 0, 0, 0, 0, 0.5};
 
 s8 gfdStreamReadInt8(gfdStream* pStream) {
     SHK_FUNCTION_CALL_1( 0x87853c, s8, gfdStream*, pStream);
@@ -58,6 +61,10 @@ u32 gfdPropertyStreamRead(gfdStream* pStream) {
     SHK_FUNCTION_CALL_1(0x876480, u32, gfdStream*, pStream);
 }
 
+void* gfdMemAllocCore(u32 size, u32 hint, char* file, u32 line) {
+    SHK_FUNCTION_CALL_4(0x884414, void*, u32, size, u32, hint, char*, file, u32, line);
+}
+
 static u32 initReadUpdateHook( gfdTask* id, float deltaTime ) {
     u32 ret = SHK_CALL_HOOK( initReadUpdate, id, deltaTime );
     return ret;
@@ -71,6 +78,7 @@ static u32 fldMainUpdateHook( gfdTask* id, float deltaTime ) {
     return SHK_CALL_HOOK( fldMainUpdate, id, deltaTime );
 }
 
+// serialize animations with user properties
 static u32 gfdAnimationStreamReadHook(gfdStream* pStream) {
     if (pStream != NULL && pStream->accessType & 1 != 0 && pStream->type == 3) {
         // gfd doesn't like us invoking their stream functions in the hook,
@@ -89,6 +97,18 @@ static u32 gfdAnimationStreamReadHook(gfdStream* pStream) {
     }
 }
 
+// fix crash for camp persona menu
+static void shdPersonaModelUpdateHook(_shd_persona_disp_control_type* work) {
+    if (work->pResPerModel != NULL) {
+        //int32_t persona = work->PersonaID;
+        if (work->pPMMDHandle == NULL) {
+            printf("pPMMDHandle is null, setting to new value 0x%x\n", shdPersonaStats);
+            work->pPMMDHandle = shdPersonaStats;
+        }
+        SHK_CALL_HOOK(shdPersonaModelUpdate, work);
+    }
+}
+
 // The start function of the PRX. This gets executed when the loader loads the PRX at boot.
 // This means game data is not initialized yet! If you want to modify anything that is initialized after boot,
 // hook a function that is called after initialisation.
@@ -96,6 +116,7 @@ void p52014Init( void ) {
     SHK_BIND_HOOK( initReadUpdate, initReadUpdateHook );
     SHK_BIND_HOOK( fldMainUpdate, fldMainUpdateHook );
     SHK_BIND_HOOK( gfdAnimationStreamRead, gfdAnimationStreamReadHook );
+    SHK_BIND_HOOK( shdPersonaModelUpdate, shdPersonaModelUpdateHook );
 }
 
 void p52014Shutdown( void )
--- prx/modules/xrd664/p5_2014.h
+++ prx/modules/xrd664/p5_2014.h
@@ -8,10 +8,12 @@
 
 // Globals
 
+typedef char gfdBool;
 typedef int16_t gfdInt16;
 typedef uint16_t gfdUInt16;
 typedef int32_t gfdInt32;
 typedef uint32_t gfdUInt32;
+typedef float gfdFloat;
 
 typedef gfdUInt32 gfdTaskState;
 
@@ -32,6 +34,20 @@ typedef gfdUInt32 gfdPlatformType;
 typedef gfdUInt16 gfdStreamType;
 
 typedef gfdUInt16 gfdStreamAccessType;
+
+typedef struct  {
+    float r;
+    float i;
+    float j;
+    float k;
+} VmathQuat;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+    float w;
+} VmathVector3;
 
 typedef struct {
     gfdNameFlag flags;
@@ -112,6 +128,63 @@ typedef struct {
     gfdUInt32 memorySize;
     uint8_t* pBuffer;
 } gfdStream;
+
+typedef struct {
+    gfdFloat posX;
+    gfdFloat posY;
+    gfdFloat posZ;
+    gfdFloat rotX;
+    gfdFloat rotY;
+    gfdFloat rotZ;
+    gfdFloat scale;
+} shdPMDD;
+
+typedef struct {
+    gfdInt32 personaID;
+    gfdUInt32 rgba;
+    gfdUInt32 sdwRgba;
+    gfdInt32 cnt;
+    gfdBool sdwFlag;
+    undefined field5_0x11;
+    undefined field6_0x12;
+    undefined field7_0x13;
+    gfdFloat depth;
+    gfdFloat dlt;
+    gfdBool texFlag;
+    undefined field11_0x1d;
+    undefined field12_0x1e;
+    undefined field13_0x1f;
+    gfdInt32 flag;
+    //Resrc_t *pResPerModel;
+    void* pResPerModel;
+    undefined field16_0x28;
+    undefined field17_0x29;
+    undefined field18_0x2a;
+    undefined field19_0x2b;
+    undefined field20_0x2c;
+    undefined field21_0x2d;
+    undefined field22_0x2e;
+    undefined field23_0x2f;
+    VmathVector3 pos;
+    VmathVector3 fromPos;
+    VmathVector3 toPos;
+    VmathVector3 rot;
+    VmathVector3 fromRot;
+    VmathVector3 toRot;
+    shdPMDD* pPMMDHandle;
+    undefined field31_0x94;
+    undefined field32_0x95;
+    undefined field33_0x96;
+    undefined field34_0x97;
+    undefined field35_0x98;
+    undefined field36_0x99;
+    undefined field37_0x9a;
+    undefined field38_0x9b;
+    undefined field39_0x9c;
+    undefined field40_0x9d;
+    undefined field41_0x9e;
+    undefined field42_0x9f;
+} _shd_persona_disp_control_type;
 
 // Functions
 void p52014Init(void);

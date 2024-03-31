df7a47902647b09ad4a0a89282d07c28356b54ea
Rirurin <rirurin@rirurin.com>
2024-03-31 20:13:58 +0800

    palace work

--- prx/modules/xrd664/p5_2014.c
+++ prx/modules/xrd664/p5_2014.c
@@ -26,8 +26,22 @@ SHK_HOOK( u32, initReadUpdate, gfdTask* id, float deltaTime );
 SHK_HOOK( u32, fldMainUpdate, gfdTask* id, float deltaTime );
 SHK_HOOK( u32, gfdAnimationStreamRead, gfdStream* pStream);
 SHK_HOOK( void, shdPersonaModelUpdate, _shd_persona_disp_control_type* work);
+SHK_HOOK( char, fldCrowdLayoutReadSync, fldCrowdLayoutHeader* pData, char await );
+//SHK_HOOK( int, btlActionParticipateStart, Participate* this, Package* pkg );
+SHK_HOOK( char, syncRoadmapFixedData, roadmap* pWork );
+SHK_HOOK( int, fldPanelRoadMapGetLayerNum, gfdTask* id );
+SHK_HOOK( char, datGetFlag, int flagNo );
+SHK_HOOK( char, btlAiXlsTablebtlAICheckConditional, void* this, void* spAction, u32 chara, u32 conditional );
+
+// persona 5
+// releasing winter 2014 for playstation 3
 
 shdPMDD shdPersonaStats[464] = {0, 0, 0, 0, 0, 0, 0.5};
+u32 pRoadmapCount = 0;
+u32 pTextureCount = 0;
+
+btlXlsTableEntry* btlAIConditionalTable = (btlXlsTableEntry*)0x11cdb64; // 92 entries
+btlXlsTableEntry* btlAIConditionalTableUnique = (btlXlsTableEntry*)0x11cdfb4; // 29 entries
 
 s8 gfdStreamReadInt8(gfdStream* pStream) {
     SHK_FUNCTION_CALL_1( 0x87853c, s8, gfdStream*, pStream);
@@ -63,6 +77,38 @@ u32 gfdPropertyStreamRead(gfdStream* pStream) {
 
 void* gfdMemAllocCore(u32 size, u32 hint, char* file, u32 line) {
     SHK_FUNCTION_CALL_4(0x884414, void*, u32, size, u32, hint, char*, file, u32, line);
+}
+
+int readEndPackFile(PAK_FILE* file) {
+    SHK_FUNCTION_CALL_1(0x1627e4, int, PAK_FILE*, file);
+}
+
+int readCheckPakFile(PAK_FILE* file) {
+    SHK_FUNCTION_CALL_1(0x162790, int, PAK_FILE*, file);
+}
+
+int16_t ririReadShort(uint8_t* buffer, u32* cursor) {
+    int16_t ret = *(int16_t*)(buffer + *cursor);
+    *cursor += 2;
+    return ret;
+}
+
+uint16_t ririReadUShort(uint8_t* buffer, u32* cursor) {
+    uint16_t ret = *(uint16_t*)(buffer + *cursor);
+    *cursor += 2;
+    return ret;
+}
+
+int ririReadInt(uint8_t* buffer, u32* cursor) {
+    int ret = *(int*)(buffer + *cursor);
+    *cursor += 4;
+    return ret;
+}
+
+u32 ririReadUInt(uint8_t* buffer, u32* cursor) {
+    u32 ret = *(u32*)(buffer + *cursor);
+    *cursor += 4;
+    return ret;
 }
 
 static u32 initReadUpdateHook( gfdTask* id, float deltaTime ) {
@@ -109,6 +155,115 @@ static void shdPersonaModelUpdateHook(_shd_persona_disp_control_type* work) {
     }
 }
 
+/*
+// clt deserialization CLT
+static char fldCrowdLayoutReadSyncHook(fldCrowdLayoutHeader* pData, char await) {
+    if (pData != NULL && pData->hFile != NULL) {
+        PAK_FILE* file = pData->hFile;
+        if (await == 1) {
+            readEndPackFile(pData->hFile);
+            file = pData->hFile;
+        }
+        int status = readCheckPakFile(file);
+        if (status == 0) {
+            return 0;
+        }
+        uint8_t* buffer = pData->hFile->info.buff;
+        u32 vCursor = 0;
+        pData->version = ririReadUInt(buffer, &vCursor);
+        pData->num = ririReadUInt(buffer, &vCursor);
+        if (pData->num != 0) {
+            void* alloc = gfdMemAllocCore(pData->num * 0x44, 0, __FILE__, __LINE__);
+            pData->pLayout = alloc;
+        }
+    }
+    return 1;
+    // if (pData != NULL) {
+    //     printf("fldCrowdLayoutRead: Data at 0x%x, version %d, %d entries\n", pData, pData->version, pData->num);
+    // }
+    // return SHK_CALL_HOOK(fldCrowdLayoutReadSync, pData, await);
+    
+}
+*/
+
+static char fldCrowdLayoutReadSyncHook(fldCrowdLayoutHeader* pData, char await) {
+    return SHK_CALL_HOOK(fldCrowdLayoutReadSync, pData, await);
+}
+
+static char syncRoadmapFixedDataHook(roadmap* pWork) {
+    char ret = SHK_CALL_HOOK(syncRoadmapFixedData, pWork);
+    /*
+    if (pWork->hRoadmapTbl != NULL) {
+        u32 bufAddr = (u32)pWork->hRoadmapTbl->info.buff;
+        pRoadmapCount = pWork->hRoadmapTbl->info.size / sizeof(fldPanelRoadmap);
+        printf("Roadmap TBL: %s, size 0x%x @ 0x%x\n", pWork->hRoadmapTbl->info.filename, pWork->hRoadmapTbl->info.size, bufAddr);
+    }
+    if (pWork->hTexPackTbl != NULL) {
+        pTextureCount = pWork->hTexPackTbl->info.size / sizeof(fldPanelRoadmapTex);
+    }
+    if (pWork->pRoadmap != NULL) {
+        for (int i = 0; i < pRoadmapCount; i++) {
+            fldPanelRoadmap* currMap = &pWork->pRoadmap[i];
+            printf("Roadmap %d: major %d, minor %d, origin (%d, %d), id %d\n", i, currMap->major, currMap->minor, currMap->origin_x, currMap->origin_y, currMap->pack_id);
+        }
+    }
+    if (pWork->pTexPack != NULL) {
+        for (int i = 0; i < pTextureCount; i++) {
+            fldPanelRoadmapTex* currTex = &pWork->pTexPack[i];
+            u32 currTexAddr = (u32)currTex;
+            printf("Texpack %d @ 0x%x: major %d, minor %d, layer %d pxl length %f flag %d\n", i, currTexAddr, currTex->major, currTex->id, currTex->layer_num, currTex->pxl_length, currTex->flag);
+        }
+    }
+    */
+    /*
+    if (pWork->hTexPackTbl != NULL) {
+        //pWork->hRoadmapTbl->info.buff
+        u32 bufAddr = (u32)pWork->hTexPackTbl->info.buff;
+        printf("TexPack TBL: %s, size 0x%x @ 0x%x\n", pWork->hTexPackTbl->info.filename, pWork->hTexPackTbl->info.size, bufAddr);
+    }*/
+    // roadmap tbl - 0xc per entry
+    // texpack tbl - 0x10 per emtry
+    return ret;
+}
+
+// it tries to access roadmap->pTexPack but that's nullptr for some reason idk
+// it's just a copy of roadmap->pTexPackTbl (see syncRoadmapFixedData) so use that as a fallback
+static int fldPanelRoadMapGetLayerNumHook(gfdTask* id) {
+    if (id == NULL) {
+        printf("WARNING: roadmap task is nullptr, returning 0 for roadmap layer\n");
+        return 0;
+    }
+    roadmap* pWork = (roadmap*)id->workData;
+    if (pWork == NULL) {
+        printf("WARNING: roadmap task work returned nullptr, returning 0 for roadmap layer\n");
+        return 0;
+    }
+    fldPanelRoadmapTex* firstTex = pWork->pTexPack;
+    if (firstTex == NULL) {
+        firstTex = pWork->pTexPackTbl;
+    }
+    return firstTex->layer_num;
+    //printf("fldPanelRoadMap Layer: Task %s, work @ 0x%x\n", id->name.string, workAddr);
+    //return SHK_CALL_HOOK(fldPanelRoadMapGetLayerNum, id);
+}
+
+// force zero when receiving an erroneus flag. roadmap stuff is broken
+static char datGetFlagHook(int flagNo) {
+    //printf("Getting flag 0x%x\n", flagNo);
+    if (flagNo < 0 || flagNo > 0x22ff) {
+        return 0;
+    }
+    return SHK_CALL_HOOK(datGetFlag, flagNo);
+}
+
+// if an xls table entry has a null function, the original function won't stop, it'll just try to deref it lol
+static char btlAiXlsTablebtlAICheckConditionalHook(void* this, void* spAction, u32 chara, u32 conditional) {
+    u32 condTblId = conditional >> 0x18;
+    u32 condArg = conditional & 0xffffff;
+    printf("AIXLSTABLE: character %d, cond table id %x, arg %x\n", chara, condTblId, condArg);
+    return SHK_CALL_HOOK(btlAiXlsTablebtlAICheckConditional, this, spAction, chara, conditional);
+}
+
 // The start function of the PRX. This gets executed when the loader loads the PRX at boot.
 // This means game data is not initialized yet! If you want to modify anything that is initialized after boot,
 // hook a function that is called after initialisation.
@@ -117,6 +272,11 @@ void p52014Init( void ) {
     SHK_BIND_HOOK( fldMainUpdate, fldMainUpdateHook );
     SHK_BIND_HOOK( gfdAnimationStreamRead, gfdAnimationStreamReadHook );
     SHK_BIND_HOOK( shdPersonaModelUpdate, shdPersonaModelUpdateHook );
+    SHK_BIND_HOOK( fldCrowdLayoutReadSync, fldCrowdLayoutReadSyncHook );
+    SHK_BIND_HOOK( syncRoadmapFixedData, syncRoadmapFixedDataHook );
+    SHK_BIND_HOOK( fldPanelRoadMapGetLayerNum, fldPanelRoadMapGetLayerNumHook );
+    SHK_BIND_HOOK( datGetFlag, datGetFlagHook );
+    SHK_BIND_HOOK( btlAiXlsTablebtlAICheckConditional, btlAiXlsTablebtlAICheckConditionalHook );
 }
 
 void p52014Shutdown( void )
--- prx/modules/xrd664/p5_2014.h
+++ prx/modules/xrd664/p5_2014.h
@@ -50,13 +50,40 @@ typedef struct {
 } VmathVector3;
 
 typedef struct {
+    gfdFloat x;
+    gfdFloat y;
+} gfdCommonVec2;
+
+typedef struct {
+    gfdFloat x;
+    gfdFloat y;
+    gfdFloat z;
+} gfdCommonVec3;
+
+typedef struct {
+    gfdFloat x;
+    gfdFloat y;
+    gfdFloat z;
+    gfdFloat w;
+} gfdCommonVec4;
+
+typedef struct {
+    gfdFloat x;
+    gfdFloat y;
+    gfdFloat w;
+    gfdFloat h;
+} gfdRect;
+
+typedef struct {
     gfdNameFlag flags;
     gfdChar *string;
     gfdInt32 length;
     gfdUInt32 hash;
 } gfdName;
 
-typedef struct {
+typedef struct gfdTask gfdTask;
+
+struct gfdTask {
     gfdTaskState state;
     gfdUInt32 flags;
     gfdTaskPrio updatePrio;
@@ -73,16 +100,20 @@ typedef struct {
     void* renderFunc;
     void* shutdownFunc;
     void *workData;
-    /*
-    gfdTaskLink link;
-    gfdTaskList idle;
-    gfdTaskList update;
-    gfdTaskList render;
-    gfdTaskList pad;
-    gfdTask *detach;
+    gfdTask* link_parent;
+    gfdTask* link_child;
+    gfdTask* link_next;
+    gfdTask* idle_prev;
+    gfdTask* idle_next;
+    gfdTask* update_prev;
+    gfdTask* update_next;
+    gfdTask* render_prev;
+    gfdTask* render_next;
+    gfdTask* pad_prev;
+    gfdTask* pad_next;
+    gfdTask* detach;
     gfdUID uid;
-    */
-} gfdTask;
+};
 
 typedef struct {
     gfdUID procUID;
@@ -185,6 +216,159 @@ typedef struct {
     undefined field41_0x9e;
     undefined field42_0x9f;
 } _shd_persona_disp_control_type;
+
+typedef struct {
+    gfdInt32 type;
+    gfdInt32 mode;
+    gfdChar filename[128];
+    gfdUInt32 size;
+    gfdInt16 maxIdx;
+    gfdInt16 freeCnt;
+    gfdInt16 refCnt;
+    gfdInt16 field92; // (padding)
+    void *buff;
+    gfdInt32 fd;
+    //CriFsLoaderHn loader;
+    void* loader;
+} PAK_FILE_INFO;
+
+typedef struct {
+    PAK_FILE_INFO info;
+    void *prv;
+    void *next;
+} PAK_FILE;
+
+typedef struct {
+    gfdUInt32 version;
+    gfdUInt32 num;
+    void* pLayout;
+    PAK_FILE* hFile;
+} fldCrowdLayoutHeader;
+
+typedef struct {
+    gfdUInt16 major;
+    gfdUInt16 minor;
+    gfdUInt16 envMajor;
+    gfdUInt16 envMinor;
+    gfdUInt16 envSub;
+    gfdUInt16 divIndex;
+    gfdUInt16 posIndex;
+    gfdUInt16 atDngIndex;
+    gfdUInt16 buildID;
+    gfdUInt16 clTotalDay;
+    gfdUInt16 clTime;
+    gfdUInt16 clWeather;
+} fldMainParam;
+
+typedef enum {
+    roadmapState_TEX_READY=1,
+    roadmapState_TEX_READ=2,
+    roadmapState_WHOLE=4,
+    roadmapState_WHOLE_FRAME=8,
+    roadmapState_VISIBLE=16,
+    roadmapState_INDEPENDENT=32,
+    roadmapState_MASKDRAW=64,
+    roadmapState_FRAMEIN=128,
+    roadmapState_FRAMEIN_ANIM=256,
+    roadmapState_FRAMEOUT_ANIM=512,
+    roadmapState_TEX_READ_PAUSE=1024,
+    roadmapState_TEX_CHANGE=2048,
+    roadmapState_ALPHA_INC=4096,
+    roadmapState_ALPHA_DEC=8192,
+    roadmapState_DEBUG=2147483648
+} roadmapState;
+
+typedef struct {
+    gfdInt16 major;
+    gfdInt16 id;
+    gfdUInt32 layer_num;
+    gfdFloat pxl_length;
+    gfdUInt32 flag;
+} fldPanelRoadmapTex;
+
+typedef struct {
+    gfdInt16 major;
+    gfdInt16 minor;
+    gfdInt16 origin_x;
+    gfdInt16 origin_y;
+    gfdInt32 pack_id;
+} fldPanelRoadmap;
+
+typedef struct {
+    gfdFloat u;
+    gfdFloat v;
+    gfdFloat w;
+    gfdFloat h;
+    gfdFloat x;
+    gfdFloat y;
+    gfdUInt32 flag;
+    gfdFloat anim_time;
+} fldPanelRoadmapParts;
+
+typedef struct {
+    gfdUInt32 step;
+    roadmapState state;
+    PAK_FILE* hRoadmapTbl;
+    //fldPanelRoadmap_t *pRoadmapTbl;
+    fldPanelRoadmap* pRoadmapTbl;
+    PAK_FILE* hTexPackTbl;
+    //fldPanelRoadmapTex_t *pTexPackTbl;
+    fldPanelRoadmapTex* pTexPackTbl;
+    PAK_FILE* hPartsTbl;
+    //fldPanelRoadmapParts_t *pPartsTbl;
+    void* pPartsTbl;
+    PAK_FILE* hDispTbl;
+    //fldPanelRoadmapParts_t *pDispTbl;
+    void* pDispTbl;
+    //fldPanelRoadmap_t *pRoadmap;
+    //fldPanelRoadmapTex_t *pTexPack;
+    //fldPanelRoadmapParts_t *pParts;
+    //fldPanelRoadmapParts_t *pDisp;
+    //fldPanelRoadmapParts_t *pCurrentParts;
+    //fldPanelRoadmapParts_t *pCurrentDisp;
+    fldPanelRoadmap *pRoadmap;
+    fldPanelRoadmapTex *pTexPack;
+    void *pParts;
+    void *pDisp;
+    void *pCurrentParts;
+    void *pCurrentDisp;
+    //gfdTexture *pTex;
+    void* pTex;
+    gfdFloat alpha_ratio;
+    gfdFloat alpha_2_ratio;
+    uint32_t field4c;
+    VmathVector3 playerSymbolGeom[6];
+    gfdFloat pxlLength;
+    gfdInt32 currentLayer;
+    gfdRect baseRC;
+    gfdRect dispRC;
+    gfdRect clipRC;
+    gfdInt32 layer_order;
+    gfdCommonVec2 maskCircleCenter;
+    gfdFloat maskCircleRadius;
+    gfdCommonVec2 anim_rotCenter;
+    gfdFloat anim_rot;
+    gfdFloat anim_time;
+    //fldMainWork_t *pParentWork;
+    void* pParentWork;
+    fldMainParam pFieldParam;
+    //gfdTaskID pPadLog;
+    gfdTask* pPadLog;
+    gfdInt32 debug_parts_cursor;
+    uint32_t field12c;
+} roadmap;
+
+typedef struct {
+    void* func;
+    char* name;
+    void* field08;
+} btlXlsTableEntry;
+
+typedef struct  {
+    int* vtable;
+    struct SmartPointer *m_pPrev;
+    struct SmartPointer *m_pNext;
+} SmartPointer;
 
 // Functions
 void p52014Init(void);
